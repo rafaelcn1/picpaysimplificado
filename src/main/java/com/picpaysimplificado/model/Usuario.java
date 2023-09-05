93c7e808e25c1199d9ae9f6a19c2ec318a2da837
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-05 19:13:06 -0300

    feat: Criação do model usuario

---
+++ src/main/java/com/picpaysimplificado/model/Usuario.java
@@ -0,0 +1,28 @@
+package com.picpaysimplificado.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String nomeCompleto;
+
+	@Column(unique = true)
+	private String documento;
+
+	@Column(unique = true)
+	private String email;
+
+	private String password;
+
+}
