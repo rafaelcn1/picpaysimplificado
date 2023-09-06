5b0d00021de941a9fe586068cd54b23756495d6f
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-06 10:18:45 -0300

    fix: ADicionado a anotação Enumerated do tipo de usuário

--- src/main/java/com/picpaysimplificado/model/Usuario.java
+++ src/main/java/com/picpaysimplificado/model/Usuario.java
@@ -2,6 +2,8 @@ package com.picpaysimplificado.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,7 +12,8 @@ import lombok.Data;
 
 @Entity
 @Data // Parar Criar todos os gets, sets, equals, toString e Hashcode
-@AllArgsConstructor // Parar criar o construtor que aceita todos os campos da classe como argumentos.
+@AllArgsConstructor // Parar criar o construtor que aceita todos os campos da classe como
+					// argumentos.
 public class Usuario {
 
 	@Id
@@ -26,9 +29,10 @@ public class Usuario {
 	private String email;
 
 	private String password;
-	
+
+	@Enumerated(EnumType.STRING) // Para dizer que o campo é um dos valores Comum ou Lojista
 	private TipoUsuario tipoUsuario;
-	
+
 	private Double carteira;
 
 }
