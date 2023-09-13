2080eb4e01b01607c71b3548e649680b88133636
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-13 14:49:16 -0300

    fix: adicionado a anotação para criação de construtores sem argumentos

--- src/main/java/com/picpaysimplificado/dto/UsuarioDTO.java
+++ src/main/java/com/picpaysimplificado/dto/UsuarioDTO.java
@@ -8,9 +8,11 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
 
 	private String nomeCompleto;
