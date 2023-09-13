821871501e76e1222b538c91034c6c77beacc62a
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-13 09:13:43 -0300

    fix: implementação do serializable e mudança do valor para BigDecimal

--- src/main/java/com/picpaysimplificado/model/Transferencia.java
+++ src/main/java/com/picpaysimplificado/model/Transferencia.java
@@ -1,6 +1,10 @@
 package com.picpaysimplificado.model;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+
+import com.picpaysimplificado.dto.TransferenciaDTO;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,26 +14,39 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
-public class Transferencia {
-	
+@NoArgsConstructor
+public class Transferencia implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
-	private Double valor;
-	
+
+	private BigDecimal valor;
+
 	@ManyToOne
 	@JoinColumn(name = "origem_id")
 	private Usuario origem;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "destino_id")
 	private Usuario destino;
-	
+
 	private LocalDateTime timeStamp;
+	
+	public Transferencia(TransferenciaDTO transferencia) {
+		this.valor = transferencia.getValor();
+		this.origem = transferencia.getOrigem();
+		this.destino = transferencia.getDestino();
+		this.timeStamp = transferencia.getTimeStamp();
+	}
 
 }
